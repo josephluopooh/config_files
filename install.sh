8800f6a568b0fa238463631d492dae7112cc024b
Jun Luo <luojun.personal@outlook.com>
2023-10-27 17:40:11 +0800

    Remove folder if already exists

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd $HOME
 
 # install softwares
-sudo apt update && sudo apt upgrade && sudo apt install zsh git gcc gdb tmux npm
+sudo apt update && sudo apt upgrade && sudo apt install zsh git gcc gdb tmux npm curl
 
 # install tldr
 sudo npm install -g tldr
@@ -11,5 +11,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # config settings
 ln -sf "$HOME/config_files/git/.gitconfig" "$HOME/.gitconfig"
+
+if [[ -d "$HOME/.oh-my-zsh/custom" ]]; then
+    rm -r "$HOME/.oh-my-zsh/custom"
+fi
 ln -sf "$HOME/config_files/oh-my-zsh/custom" "$HOME/.oh-my-zsh/custom"
+
 ln -sf "$HOME/config_files/zsh/.zshrc" "$HOME/.zshrc"
